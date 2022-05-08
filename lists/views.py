ab4416d4ad554e85fb8ae178ac5fa1aeeb74cbfa
Cobus Linde <barendl@staycontactable.com>
2022-05-08 21:19:25 +0200

    functional test for second table item

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html')
+    # if request.method == 'POST':
+    context = {
+        'new_item_text': request.POST.get('item_text', ''),
+    }
+        # return render(request, 'home.html', context)
+
+    return render(request, 'home.html', context)
