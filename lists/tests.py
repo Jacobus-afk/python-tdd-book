32e6065dc67c279811c1f5e55fbd398a67af699b
Cobus Linde <barendl@staycontactable.com>
2022-05-09 19:12:37 +0200

    URLs for forms x2, moved code in views + tests, new URL

--- lists/tests.py
+++ lists/tests.py
@@ -41,47 +41,25 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
 class HomePageTest(TestCase):
-
-    # def test_root_url_resolves_to_home_page_view(self):
-    #     found = resolve('/')  
-    #     self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        # request = HttpRequest()
-        # response = home_page(request)
         response = self.client.get('/')
-
-        # html = response.content.decode('utf8')
-        # self.assertTrue(html.startswith('<html>'))
-        # self.assertIn('<title>To-Do lists</title>', html)
-        # self.assertTrue(html.endswith('</html>'))
-
         self.assertTemplateUsed(response, 'home.html')
 
-    # def test_displays_all_list_items(self):
-    #     Item.objects.create(text='itemey 1')
-    #     Item.objects.create(text='itemey 2')
 
-    #     response = self.client.get('/')
+    # def test_only_saves_items_when_necessary(self):
+    #     self.client.get('/')
+    #     self.assertEqual(Item.objects.count(), 0)
 
-    #     self.assertIn('itemey 1', response.content.decode())
-    #     self.assertIn('itemey 2', response.content.decode())
-
+class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-        # self.assertIn('A new list item', response.content.decode())
-        # self.assertTemplateUsed(response, 'home.html')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render, redirect
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     new_item_text = request.POST.get('item_text', '')
+    #     Item.objects.create(text=new_item_text)
+    #     return redirect('/lists/the-only-list-in-the-world/')
 
     # items = Item.objects.all()
     return render(request, 'home.html')#, {'items': items})
@@ -15,3 +15,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST.get('item_text', ''))
+    return redirect('/lists/the-only-list-in-the-world/')
