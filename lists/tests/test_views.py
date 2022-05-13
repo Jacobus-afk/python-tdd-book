e361c5f4bdb54312d0e03e57f33dabd0ac2094ad
Cobus Linde <barendl@staycontactable.com>
2022-05-13 21:24:29 +0200

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,9 +1,5 @@
-# from urllib import response
-# from django.urls import resolve
 from django.test import TestCase
-# from django.http import HttpRequest
 
-# from lists.views import home_page
 from lists.models import Item, List
 
 
@@ -11,40 +7,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-
-    # def test_only_saves_items_when_necessary(self):
-    #     self.client.get('/')
-    #     self.assertEqual(Item.objects.count(), 0)
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'First list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Second list item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'First list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Second list item')
-        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     def test_displays_only_items_for_that_list(self):
@@ -84,8 +46,6 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        # self.assertEqual(response.status_code, 302)
-        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
